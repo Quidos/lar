6c2944ad0c75f780f46b70d2e74b0d6bb4f3ae5a
quidos <tilen.kamnik@gmail.com>
2024-06-14 23:03:10 +0200

    remove unused import

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Quidos\Lar\Kernel\Http\Request;
-use Quidos\Lar\Kernel\Model\Database;
 use Quidos\Lar\Routing\Router;
 
 Dotenv::createImmutable(__DIR__ . '/..')->load();
@@ -13,5 +12,3 @@ $request = Request::fromGlobals();
 $router = new Router($request);
 $response = $router->route();
 $response->send();
-
-$db = Database::getInstance();
